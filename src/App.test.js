29db834905dfdf3900d4c2b00fffaf65096d2f7f
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-06-04 13:24:26 +0100

    Tested the api call

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import moxios from "moxios";
+import axios from "axios";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Fetch API", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+  it("fetch User profile", async () => {
+    const fetchProfile = async () => {
+      const res = await axios.get("http://localhost:4000/api/");
+      return res.data;
+    };
+    const expectedProfile = [
+      [
+        {
+          name: "Akintade Ajibade",
+          jobTitle: "Senior Frontend Developer",
+          favouriteFood: "Bolognese Pasta",
+          pictureUrl: "/profile.jpg",
+        },
+      ],
+    ];
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({ status: 200, response: expectedProfile });
+    });
+
+    const result = await fetchProfile();
+    expect(result).toEqual(expectedProfile);
+  });
 });
