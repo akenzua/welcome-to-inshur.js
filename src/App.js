044c83e02ddfb7319d9ffdbbbe2caa6fac407d56
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-06-04 13:05:27 +0100

    cleaned up frontend src folder and added hooks to consume api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [profile, setProfile] = useState([]);
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const res = await axios.get("http://localhost:4000/api/");
+      setProfile(res.data);
+    };
+    fetchProfile();
+  }, []);
+  return <div className="App">Profile</div>;
+};
 
 export default App;
