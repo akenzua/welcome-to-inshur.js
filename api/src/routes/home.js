b52c6ca741b8bd186faf0fda3d8714a1b1d28409
Akintade Ajibade <ajibade.akintade@gmail.com>
2020-06-04 13:01:24 +0100

    Update the backend to send user details

--- api/src/routes/home.js
+++ api/src/routes/home.js
@@ -1,11 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const home = Router();
 
-home.get('/', (request, response) => {
-  response.send({
-    success: true
-  })
+home.get("/", (request, response) => {
+  response.send([
+    {
+      name: "Akintade Ajibade",
+      jobTitle: "Senior Frontend Developer",
+      favouriteFood: "Bolognese Pasta",
+      pictureUrl: "/profile.jpg",
+    },
+  ]);
 });
 
- module.exports = home;
+module.exports = home;
